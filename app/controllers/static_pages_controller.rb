325ffcd2139123a9ced200faff09944f46ef355f
Andy Perez <agperez@gmail.com>
2014-04-17 13:50:45 -0400

    V1.0.1 Heroku bug fixes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @users = User.all
+    @hasPortfolio = User.joins(:portfolio).uniq.load
+    @users = @hasPortfolio.where(admin: nil)
   	if signed_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
